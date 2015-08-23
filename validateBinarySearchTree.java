f9bc26201337c2a9b794efa308d3f5e7f2e37029
Success2014 <neogoyeah@gmail.com>
2015-08-22 23:49:31 -0500

    v1

---
+++ validateBinarySearchTree.java
@@ -0,0 +1,48 @@
+package first;
+
+public class validateBinarySearchTree {
+	/**
+	 * Method 1, set a upper and lower bound for each node, check if each node is in the bounds.
+	 * @param root
+	 * @return
+	 */
+	public boolean isValidBST(TreeNode root) {
+		return validBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+	public boolean validBST(TreeNode node, long low, long high){
+        if (node == null) return true;        
+        return node.val > low && node.val < high && validBST(node.left, low, node.val) && validBST(node.right, node.val, high);
+    }
+	
+	/**
+	 * Method 2, in-order traversal. Check sequentially if the previous node is less than the current node.
+	 */
+	private TreeNode prev;
+	public boolean isValidBST2(TreeNode root) {
+		prev = null;
+		return isMonoIncrease(root);
+	}
+
+	public boolean isMonoIncrease(TreeNode p){
+	    if (p == null) return true;
+	    if (isMonoIncrease(p.left)){
+	        if (prev != null && prev.val >= p.val) return false;
+	        prev = p;
+	        return isMonoIncrease(p.right);
+	    }
+	    return false;
+	}
+	
+	
+	
+    
+    public static void main (String[] args){
+    	validateBinarySearchTree vbst = new validateBinarySearchTree();
+    	TreeNode node1 = new TreeNode(0);
+    	System.out.println(vbst.isValidBST(node1));
+    	TreeNode node2 = new TreeNode(2147483647);
+    	System.out.println(vbst.isValidBST(node2));
+    }
+
+}
