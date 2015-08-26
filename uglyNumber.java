0eb5223f4045c714dc9fe3eadf8feef0617e003d
Success2014 <neogoyeah@gmail.com>
2015-08-26 10:28:52 -0500

    v1

---
+++ uglyNumber.java
@@ -0,0 +1,14 @@
+package first;
+
+public class uglyNumber {
+	public boolean isUgly(int num) {
+        while (num > 1){
+            if (num % 2 == 0) num /= 2;
+            else if (num % 3 == 0) num /= 3;
+            else if (num % 5 == 0) num /= 5;
+            else return false;
+        }
+        return num == 1;
+    }
+
+}
