5e878051d652896f2f021179429ad491bf3fcb5b
Success2014 <neogoyeah@gmail.com>
2015-09-09 12:17:48 -0500

    v1

---
+++ interleavingString.java
@@ -0,0 +1,39 @@
+package first;
+
+public class interleavingString {
+	public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        int k = s3.length();
+        
+        if (m + n != k){
+            return false;
+        }
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        
+        for (int i = 1; i <= m; i++){
+            if (s1.charAt(i-1) == s3.charAt(i-1) && dp[i-1][0]){
+                dp[i][0] = true;
+            }
+        }
+        
+        for (int j = 1; j <= n; j++){
+            if (s2.charAt(j-1) == s3.charAt(j-1) && dp[0][j-1]){
+                dp[0][j] = true;
+            }
+        }
+        
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                boolean cond1 = dp[i-1][j] && (s1.charAt(i-1) == s3.charAt(i+j-1));
+                boolean cond2 = dp[i][j-1] && (s2.charAt(j-1) == s3.charAt(i+j-1));
+                if (cond1 || cond2){
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+}
