78b1b94c8812473f0e9050ba3c8f47b2d1ad1f52
Success2014 <neogoyeah@gmail.com>
2015-09-09 10:51:53 -0500

    v1

---
+++ distinctSubsequences.java
@@ -0,0 +1,36 @@
+package first;
+
+public class distinctSubsequences {
+	public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 0; i <= m; i++){
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j <= n; j++){
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j<= n; j++){
+                if (s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+	
+	public static void main(String[] args){
+		distinctSubsequences ds = new distinctSubsequences();
+		String s1 = "abc"; String t1 = "abcd";
+		System.out.println(ds.numDistinct(s1, t1));
+		String s2 = "abcdde"; String t2 = "abcd";
+		System.out.println(ds.numDistinct(s2, t2));
+	}
+
+}
