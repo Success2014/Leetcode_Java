2e041ad5ef0c34ff88e125a00d942cdc62a2db80
Success2014 <neogoyeah@gmail.com>
2015-09-10 11:01:57 -0500

    v1

---
+++ perfectSquares.java
@@ -0,0 +1,20 @@
+package first;
+import java.util.Arrays;
+public class perfectSquares {
+	public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        //init
+        for (int i = 1; i * i <= n; i++){
+            dp[i*i] = 1;
+        }
+        
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; i + j * j <= n; j++){
+                dp[i + j * j] = Math.min(dp[i] + 1, dp[i + j * j]);
+            }
+        }
+        return dp[n];
+    }
+
+}
