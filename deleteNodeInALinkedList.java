02d39b408ff85a2c29388594409a3917f4df5f92
Success2014 <neogoyeah@gmail.com>
2015-09-12 13:29:21 -0500

    v1

---
+++ deleteNodeInALinkedList.java
@@ -0,0 +1,9 @@
+package first;
+
+public class deleteNodeInALinkedList {
+	public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
