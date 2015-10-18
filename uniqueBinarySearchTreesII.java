d7b371fee40a1e8ca634b1fb6966d6c490a8a4f2
Success2014 <neogoyeah@gmail.com>
2015-10-17 22:32:40 -0500

    v1

---
+++ uniqueBinarySearchTreesII.java
@@ -0,0 +1,32 @@
+package first;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class uniqueBinarySearchTreesII {
+	public List<TreeNode> generateTrees(int n) {
+        return helper(1, n);
+    }
+    public List<TreeNode> helper(int start, int end){
+        List<TreeNode> res = new ArrayList<TreeNode>();
+        if (start > end){
+            res.add(null);
+            return res;
+        }
+        
+        for (int i = start; i <= end; i++){
+            List<TreeNode> lefts = helper(start, i - 1);
+            List<TreeNode> rights = helper(i + 1, end);
+            for (TreeNode left: lefts){
+                for (TreeNode right: rights){
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+
+}
