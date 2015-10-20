e3983577e0f41b9f518c6e9962058c1f2aa9da16
Success2014 <neogoyeah@gmail.com>
2015-10-19 21:04:19 -0500

    v1

--- powxn.java
+++ powxn.java
@@ -31,11 +31,34 @@ public class powxn {
         }
     }
 	
+	/**
+	 * Iterative solution
+	 * @param x
+	 * @param n
+	 * @return
+	 */
+	public double myPow2(double x, int n) {
+        double res = 1.0;
+        boolean neg = (n < 0);
+        n = Math.abs(n);
+        while (n > 0){
+            if (n % 2 == 0){
+                x *= x;
+                n /= 2;
+            }
+            else{
+                n -= 1;
+                res *= x;
+            }
+        }
+        return neg ? 1/res : res;
+    }
+	
 	public static void main(String[] args){
 		powxn px = new powxn();
-		double x = 1.0;
+		double x = 2.0;
 		int n = -2147483648;
-		System.out.println(px.myPow(x, n));
+		System.out.println(px.myPow2(x, n));
 	}
 
 }
