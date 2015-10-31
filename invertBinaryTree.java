809f0f4a0de9a12e1d780c1f0619b132db291bf3
Success2014 <neogoyeah@gmail.com>
2015-10-31 18:20:18 -0500

    v1

--- invertBinaryTree.java
+++ invertBinaryTree.java
@@ -1,6 +1,13 @@
 package first;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class invertBinaryTree {
+	/**
+	 * Recursive
+	 * @param root
+	 * @return
+	 */
 	public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
@@ -11,5 +18,44 @@ public class invertBinaryTree {
         root.right = left;
         return root;
     }
+	/**
+	 * Iterative
+	 * @param root
+	 * @return
+	 */
+	public TreeNode invertTreeIter(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+        }
+        return root;
+    }
+	
+	public static void main(String[] args) {
+		invertBinaryTree ibt = new invertBinaryTree();
+		TreeNode nd1 = new TreeNode(1);
+		TreeNode nd2 = new TreeNode(2);
+		TreeNode nd3 = new TreeNode(3);
+		TreeNode nd4 = new TreeNode(4);
+		TreeNode nd5 = new TreeNode(5);
+		nd1.left = nd2;
+		nd1.right = nd3;
+		nd2.left = nd4;
+		nd2.right = nd5;
+		ibt.invertTreeIter(nd1);
+	}
 
 }
