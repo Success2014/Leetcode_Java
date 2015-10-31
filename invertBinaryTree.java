37bda9556a1a05768e98a1c1f86c09c7a3371bfd
Success2014 <neogoyeah@gmail.com>
2015-10-31 18:02:43 -0500

    v1

---
+++ invertBinaryTree.java
@@ -0,0 +1,15 @@
+package first;
+
+public class invertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+}
