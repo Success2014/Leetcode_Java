be5c70f0616d4f7ee0b07b305aa1bc67e22c39be
Success2014 <neogoyeah@gmail.com>
2015-11-03 13:03:39 -0600

    v1

---
+++ singleNumber2.java
@@ -0,0 +1,12 @@
+package first;
+
+public class singleNumber2 {
+	public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+
+}
