d7dc54dbc8ba067a09ba3b42d14a18db1c916a8a
Success2014 <neogoyeah@gmail.com>
2015-11-09 14:51:53 -0600

    v1

---
+++ searchInsertPosition.java
@@ -0,0 +1,26 @@
+package first;
+
+public class searchInsertPosition {
+	public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (nums[start] >= target) {
+            return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return n;
+        }
+    }
+}
