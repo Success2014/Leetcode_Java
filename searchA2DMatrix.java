0e3ff53105f0212189f6fbd1bbb7c2901ea62b4c
Success2014 <neogoyeah@gmail.com>
2015-11-09 17:27:37 -0600

    v1

---
+++ searchA2DMatrix.java
@@ -0,0 +1,59 @@
+package first;
+
+public class searchA2DMatrix {
+	public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = (m == 0) ? 0 : matrix[0].length;
+        if (m == 0 || n == 0) {
+            return false;
+        }
+        
+        int start = 0;
+        int end = m - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[mid][0] == target) {
+                return true;
+            } else if (matrix[mid][0] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        int row = 0;
+        if (matrix[end][0] <= target) {
+            row = end;
+        } else if (matrix[start][0] <= target) {
+            row = start;
+        } else {
+            return false;
+        }
+        
+        start = 0;
+        end = n - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (matrix[row][start] == target) {
+            return true;
+        } else if (matrix[row][end] == target){
+            return true;
+        } else {
+            return false;
+        }
+    }
+	public static void main(String[] args) {
+		searchA2DMatrix sm = new searchA2DMatrix();
+		int[][] m1 = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
+		System.out.println(sm.searchMatrix(m1, 5));
+	}
+}
+
