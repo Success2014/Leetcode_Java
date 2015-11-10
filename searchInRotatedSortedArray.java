6bf93f4526d703ec586a18895c2b4ddc147575e0
Success2014 <neogoyeah@gmail.com>
2015-11-10 01:51:28 -0600

    v1

---
+++ searchInRotatedSortedArray.java
@@ -0,0 +1,35 @@
+package first;
+
+public class searchInRotatedSortedArray {
+	public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > nums[start]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        if (nums[start] == target) {
+            return start;
+        } else if (nums[end] == target) {
+        	return end;
+        }
+        else {
+        	return -1;
+        }
+        
+	}
+}
