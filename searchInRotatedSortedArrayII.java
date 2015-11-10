4a9e065396d8ac34b0bc661efbdf9948f66204ab
Success2014 <neogoyeah@gmail.com>
2015-11-10 02:07:12 -0600

    v1

---
+++ searchInRotatedSortedArrayII.java
@@ -0,0 +1,13 @@
+package first;
+
+public class searchInRotatedSortedArrayII {
+	public boolean search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
