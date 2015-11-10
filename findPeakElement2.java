4ddd16269cbdb7d6b86c4fad1132391f35ef7bc1
Success2014 <neogoyeah@gmail.com>
2015-11-09 21:54:38 -0600

    v1

---
+++ findPeakElement2.java
@@ -0,0 +1,27 @@
+package first;
+
+public class findPeakElement2 {
+	public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]) {
+                start = mid;
+            } else {
+                end = mid;
+            } 
+        }
+        
+        if (nums[start] > nums[end]) {
+            return start;
+        } else {
+            return end;
+        }
+    }
+
+}
