6a9e255f0cc209c154c34a4bea3e79a9d96aeede
Success2014 <neogoyeah@gmail.com>
2015-11-11 16:19:15 -0600

    v1

---
+++ binaryTreePreorderTraversal2.java
@@ -0,0 +1,24 @@
+package first;
+import java.util.*;
+public class binaryTreePreorderTraversal2 {
+	public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+
+}
