11c4911d6a7fe155dd31453fb3e2026f1d427f9c
Success2014 <neogoyeah@gmail.com>
2015-11-11 20:35:29 -0600

    v1

---
+++ lowestCommonAncestorOfABinaryTree2.java
@@ -0,0 +1,21 @@
+package first;
+
+public class lowestCommonAncestorOfABinaryTree2 {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        }
+        return null;
+    }
+
+}
