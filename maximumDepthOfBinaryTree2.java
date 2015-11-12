1379307d754dd567302d2c4619cf23e260d4d283
Success2014 <neogoyeah@gmail.com>
2015-11-11 18:26:31 -0600

    v1

---
+++ maximumDepthOfBinaryTree2.java
@@ -0,0 +1,13 @@
+package first;
+
+public class maximumDepthOfBinaryTree2 {
+	public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+}
