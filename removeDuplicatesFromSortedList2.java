dd7642de7aab9e5983bbfb7377288b0b038d318c
Success2014 <neogoyeah@gmail.com>
2015-11-14 16:09:16 -0600

    v1

---
+++ removeDuplicatesFromSortedList2.java
@@ -0,0 +1,19 @@
+package first;
+
+public class removeDuplicatesFromSortedList2 {
+	public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+}
