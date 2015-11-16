77cf54e280546dcaf0a605255a6bc37162f64c33
Success2014 <neogoyeah@gmail.com>
2015-11-15 21:50:38 -0600

    v1

---
+++ subsets4.java
@@ -0,0 +1,24 @@
+package first;
+import java.util.*;
+public class subsets4 {
+	public List<List<Integer>> subsets(int[] nums) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        int n = nums.length;
+        if (n == 0) {
+            return res;
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, res, list, 0);
+        return res;
+    }
+    public void dfs(int[] nums, ArrayList<List<Integer>> res, ArrayList<Integer> list, int pos) {
+        res.add(new ArrayList<Integer>(list));
+        for (int i = pos; i < nums.length; i++) {
+            list.add(nums[i]);
+            dfs(nums, res, list, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
