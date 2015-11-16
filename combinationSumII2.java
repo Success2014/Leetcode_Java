932cfc106375c7042431c0d7f1d837604e58467e
Success2014 <neogoyeah@gmail.com>
2015-11-15 22:38:26 -0600

    v1

---
+++ combinationSumII2.java
@@ -0,0 +1,27 @@
+package first;
+import java.util.*;
+public class combinationSumII2 {
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        dfs(candidates, res, list, 0, target);
+        return res;
+    }
+    public void dfs(int[] nums, ArrayList<List<Integer>> res, ArrayList<Integer> list, int pos, int t) {
+        if (t < 0) {
+            return;
+        } else if (t == 0) {
+            res.add(new ArrayList<Integer>(list));
+        }
+        for (int i = pos; i < nums.length; i++) {
+            if (i != pos && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            list.add(nums[i]);
+            dfs(nums, res, list, i + 1, t - nums[i]);
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
