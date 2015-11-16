84e89aac213a9c04473d5580ef071f8606af4a12
Success2014 <neogoyeah@gmail.com>
2015-11-16 16:49:45 -0600

    v1

---
+++ uniqueBinarySearchTrees3.java
@@ -0,0 +1,16 @@
+package first;
+
+public class uniqueBinarySearchTrees3 {
+	public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 0;
+            for (int j = 0; j <= i - 1; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[n];
+    }
+
+}
