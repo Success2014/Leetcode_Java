e7cd8770e7d84fb5b18e455bf9ba4d484da75dd4
Success2014 <neogoyeah@gmail.com>
2015-11-16 20:34:13 -0600

    v1

---
+++ implementQueueUsingStacks3.java
@@ -0,0 +1,32 @@
+package first;
+import java.util.*;
+public class implementQueueUsingStacks3 {
+	Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    // Push element x to the back of queue.
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        peek();
+        stack2.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+}
