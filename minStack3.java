b9dd6c72f070f85984bef2066a832a6072ebb7f0
Success2014 <neogoyeah@gmail.com>
2015-11-16 20:12:15 -0600

    v1

---
+++ minStack3.java
@@ -0,0 +1,28 @@
+package first;
+import java.util.*;
+public class minStack3 {
+	Stack<Integer> stack = new Stack<Integer>();
+    Stack<Integer> minStack = new Stack<Integer>();
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int x = stack.pop();
+        if (x == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
