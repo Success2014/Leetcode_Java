ad5ab85c7d87a280e33fc311d18b02575f42c809
Success2014 <neogoyeah@gmail.com>
2015-11-28 00:13:53 -0600

    v1

--- trappingRainWater.java
+++ trappingRainWater.java
@@ -1,6 +1,11 @@
 package first;
 
 public class trappingRainWater {
+	/**
+	 * O(n) time, O(n) space
+	 * @param height
+	 * @return
+	 */
 	public int trap(int[] height) {
         int n = height.length;
         if (n == 0) {
@@ -22,6 +27,33 @@ public class trappingRainWater {
         int res = 0;
         for (int i = 0; i < n; i++) {
             res += Math.min(leftMostHigh[i], rightMostHigh[i]) - height[i];
+        }
+        return res;
+    }
+	
+	/**
+	 * O(n) time, O(1) space
+	 * @param height
+	 * @return
+	 */
+	public int trap2(int[] height) {
+        int n = height.length;
+        if (n <= 1) {
+            return 0;
+        }
+        int i = 0;
+        int j = n - 1;
+        int res = 0;
+        int toFill = 0;
+        while (i < j) {
+            toFill = Math.max(toFill, Math.min(height[i], height[j]));
+            if (height[i] < height[j]) {
+                res += toFill - height[i];
+                i++;
+            } else {
+                res += toFill - height[j];
+                j--;
+            }
         }
         return res;
     }
