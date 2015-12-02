a0081615d9d48b12433c78d4db66be15ffa2b734
Success2014 <neogoyeah@gmail.com>
2015-12-01 22:51:26 -0600

    v1

---
+++ nimGame2.java
@@ -0,0 +1,12 @@
+package first;
+
+public class nimGame2 {
+	public boolean canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
