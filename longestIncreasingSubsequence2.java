4819f5f39b1259133e6a761d07618b75d8e11197
Success2014 <neogoyeah@gmail.com>
2015-12-01 20:23:36 -0600

    v1

---
+++ longestIncreasingSubsequence2.java
@@ -0,0 +1,24 @@
+package first;
+
+public class longestIncreasingSubsequence2 {
+	public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+}
