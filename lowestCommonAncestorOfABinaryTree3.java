cf0fa734b11d0b629e14d54f823f7c39cc289700
Success2014 <neogoyeah@gmail.com>
2015-12-12 16:24:31 -0600

    v1

---
+++ lowestCommonAncestorOfABinaryTree3.java
@@ -0,0 +1,22 @@
+package first;
+
+public class lowestCommonAncestorOfABinaryTree3 {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+
+}
