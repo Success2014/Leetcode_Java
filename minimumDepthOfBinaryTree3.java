7d7e65bd612c76f6fb3f731b03fa771dd6b9caef
Success2014 <neogoyeah@gmail.com>
2015-12-12 15:34:12 -0600

    v1

---
+++ minimumDepthOfBinaryTree3.java
@@ -0,0 +1,17 @@
+package first;
+
+public class minimumDepthOfBinaryTree3 {
+	public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        } else if (root.right == null) {
+            return minDepth(root.left) + 1;
+        } else {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+
+}
