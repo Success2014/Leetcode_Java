3dcd5083b355d022abe5966cee05c52992d3d7f2
Success2014 <neogoyeah@gmail.com>
2015-12-14 16:33:27 -0600

    v1

---
+++ removeNthNodeFromEndOfList2.java
@@ -0,0 +1,20 @@
+package first;
+
+public class removeNthNodeFromEndOfList2 {
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+}
