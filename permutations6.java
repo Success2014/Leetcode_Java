021de99c8cfe1a1261442411f99bbccc0d2ed9fd
Success2014 <neogoyeah@gmail.com>
2015-12-14 10:58:17 -0600

    v1

---
+++ permutations6.java
@@ -0,0 +1,24 @@
+package first;
+import java.util.*;
+public class permutations6 {
+	public List<List<Integer>> permute(int[] nums) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        dfs(nums, res, list);
+        return res;
+    }
+    public void dfs(int[] nums, ArrayList<List<Integer>> res, ArrayList<Integer> list) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<Integer>(list));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) {
+                continue;
+            }
+            list.add(nums[i]);
+            dfs(nums, res, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
